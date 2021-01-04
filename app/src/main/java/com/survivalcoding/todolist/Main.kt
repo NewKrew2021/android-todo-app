93b8d5b8c81dc02fa10b7ce6fc5104c501d6fe3d
Junsuk Oh <a811219@gmail.com>
2021-01-04 15:27:03 +0900

    kotlin 과제

---
+++ app/src/main/java/com/survivalcoding/todolist/Main.kt
@@ -0,0 +1,20 @@
+package com.survivalcoding.todolist
+
+data class Trader(val name: String, val city: String)
+data class Transaction(val trader: Trader, val year: Int, val value: Int)
+
+
+val transactions = listOf(
+    Transaction(Trader("Brian", "Cambridge"), 2011, 300),
+    Transaction(Trader("Raoul", "Cambridge"), 2012, 1000),
+    Transaction(Trader("Raoul", "Cambridge"), 2011, 400),
+    Transaction(Trader("Mario", "Milan"), 2012, 710),
+    Transaction(Trader("Mario", "Milan"), 2012, 700),
+    Transaction(Trader("Alan", "Cambridge"), 2012, 950),
+)
+
+fun main() {
+    // 1
+    transactions.filter { it.trader.name.startsWith("A") }
+            .forEach { println(it) }
+}
