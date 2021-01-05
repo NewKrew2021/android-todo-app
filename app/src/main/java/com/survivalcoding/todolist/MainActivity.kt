4013e7d30a7a15c3a3808e512a31e68e9f14b1be
qufwnfahs <style_55@naver.com>
2021-01-05 20:06:34 +0900

    MainActivity 수정

--- app/src/main/java/com/survivalcoding/todolist/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/todolist/MainActivity.kt
@@ -1,24 +1,40 @@
 package com.survivalcoding.todolist
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.survivalcoding.todolist.adapter.Todo
 import com.survivalcoding.todolist.adapter.TodoAdapter
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val items = mutableListOf<Todo>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val adapter = TodoAdapter()
+        val adapter = TodoAdapter(this, items)
 
-        val listView = findViewById<ListView>(R.id.list_view)
-        listView.adapter = adapter
+        val editTitle = findViewById<EditText>(R.id.editText_title)
 
-        listView.setOnItemClickListener { _, _, position, _ ->
-            Toast.makeText(this, "$position 번째 아이템 선택", Toast.LENGTH_SHORT).show()
+        findViewById<ListView>(R.id.list_view).adapter = adapter
+
+        findViewById<Button>(R.id.button_add).setOnClickListener {
+            if (editTitle.text.trim().isNotEmpty()) {
+                items.add(
+                    Todo(
+                        editTitle.text.toString(),
+                        Calendar.getInstance().time.toString()
+                    )
+                )
+                adapter.notifyDataSetChanged()
+
+                editTitle.text.clear()
+            }
         }
     }
 }
