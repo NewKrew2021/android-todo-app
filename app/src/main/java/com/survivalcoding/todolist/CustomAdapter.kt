567deb2ca0da4950ed83de47f5bf5b89adb2a159
summer.dev <summer.dev@kakaocorp.com>
2021-01-05 17:21:23 +0900

    2021.01.05 TO-DO List

---
+++ app/src/main/java/com/survivalcoding/todolist/CustomAdapter.kt
@@ -0,0 +1,73 @@
+package com.survivalcoding.todolist
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.TextView
+
+class CustomAdapter() : BaseAdapter() {
+
+    var items = mutableListOf<TodoData>()
+
+    private fun sortItem() {
+        items = items.sortedWith(compareBy(
+            { if (it.check) 1 else 0 },
+            { if (it.mark) 0 else 1 },
+            { -it.time }
+        )).toMutableList()
+        Log.d("aaa", items.toString())
+        notifyDataSetChanged()
+    }
+
+    fun addItem(data: TodoData) {
+        items.add(data)
+        sortItem()
+    }
+
+    override fun getCount(): Int {
+        return items.size
+    }
+
+    override fun getItem(position: Int): TodoData {
+        return items[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View
+        if (convertView == null) {
+            view = LayoutInflater.from(parent!!.context)
+                .inflate(R.layout.list_item, parent, false)
+        } else {
+            view = convertView
+        }
+        val checkBox = view.findViewById<CheckBox>(R.id.checkbox)
+        checkBox.setOnClickListener {
+            items[position].check = !items[position].check
+            sortItem()
+        }
+        val markBox = view.findViewById<ImageView>(R.id.mark_box)
+        markBox.setOnClickListener {
+            items[position].mark = !items[position].mark
+            if (items[position].mark) markBox.setImageResource(R.drawable.ic_baseline_star_24)
+            else markBox.setImageResource(R.drawable.ic_baseline_star_outline_24)
+            sortItem()
+        }
+        val item = getItem(position)
+        val todoText = view.findViewById<TextView>(R.id.text_todo)
+        todoText.text = item.text
+        if (item.mark) markBox.setImageResource(R.drawable.ic_baseline_star_24)
+        else markBox.setImageResource(R.drawable.ic_baseline_star_outline_24)
+        checkBox.isChecked = item.check
+        return view
+
+    }
+}
