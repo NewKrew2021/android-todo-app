8ae982874122840807496903cc559a13ccd8ec9e
qufwnfahs <style_55@naver.com>
2021-01-05 20:05:24 +0900

    data class Todo 생성 및 TodoAdapter 수정

--- app/src/main/java/com/survivalcoding/todolist/adapter/TodoAdapter.kt
+++ app/src/main/java/com/survivalcoding/todolist/adapter/TodoAdapter.kt
@@ -1,21 +1,64 @@
 package com.survivalcoding.todolist.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.TextView
+import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.survivalcoding.todolist.R
 
-class TodoAdapter : BaseAdapter() {
+// TODO : isDone, Times 에 따른 정렬 구현
+data class Todo (
+    var title: String,
+    var times: String,
+    var isDone: Boolean = false,
+    var isOption: Boolean = false,
+)
 
-    private val items = listOf(
-        "Hi", "Hello", "Why",
-        "Hi", "Hello", "Why", "Hi", "Hello", "Why", "Hi", "Hello", "Why",
-        "Hi", "Hello", "Why", "Hi", "Hello", "Why", "Hi", "Hello", "Why",
-    )
+class TodoAdapter(
+    private val context: Context,
+    private val items: MutableList<Todo>,
+) : BaseAdapter() {
 
     class TodoViewHolder {
-        lateinit var tempText: TextView
+        lateinit var layout: ConstraintLayout
+        lateinit var textTitle: TextView
+        lateinit var textTimes: TextView
+        lateinit var checkBox: CheckBox
+        lateinit var btnMenus: ImageButton
+        lateinit var btnEdit: ImageButton
+        lateinit var btnDelete: ImageButton
+
+        fun bind(todo: Todo) {
+            textTitle.text = todo.title
+            textTimes.text = todo.times
+            checkBox.isChecked = todo.isDone
+
+            updateButtonsVisibility(todo.isOption)
+
+            checkBox.setOnClickListener {
+                todo.isDone = checkBox.isChecked
+            }
+
+            btnMenus.setOnClickListener {
+                todo.isOption = true
+                updateButtonsVisibility(todo.isOption)
+            }
+
+            layout.setOnClickListener {
+                if (todo.isOption) {
+                    todo.isOption = false
+                    updateButtonsVisibility(todo.isOption)
+                }
+            }
+        }
+
+        private fun updateButtonsVisibility(isOption: Boolean) {
+            btnMenus.visibility = if (isOption) View.INVISIBLE else View.VISIBLE
+            btnEdit.visibility = if (isOption) View.VISIBLE else View.INVISIBLE
+            btnDelete.visibility = if (isOption) View.VISIBLE else View.INVISIBLE
+        }
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
@@ -23,11 +66,19 @@ class TodoAdapter : BaseAdapter() {
         val holder: TodoViewHolder
 
         if (convertView == null) {
-            view = LayoutInflater.from(parent!!.context)
-                .inflate(android.R.layout.simple_list_item_1, parent, false)
+            view = LayoutInflater.from(context)
+                .inflate(R.layout.item_todo_list, parent, false)
 
             holder = TodoViewHolder()
-            holder.tempText = view.findViewById(android.R.id.text1)
+            holder.apply {
+                layout = view.findViewById(R.id.layout_item)
+                textTitle = view.findViewById(R.id.textView_title)
+                textTimes = view.findViewById(R.id.textView_times)
+                checkBox = view.findViewById(R.id.checkBox)
+                btnMenus = view.findViewById(R.id.button_menus)
+                btnEdit = view.findViewById(R.id.button_edit)
+                btnDelete = view.findViewById(R.id.button_delete)
+            }
 
             view.tag = holder
         } else {
@@ -35,12 +86,24 @@ class TodoAdapter : BaseAdapter() {
             holder = (view.tag) as TodoViewHolder
         }
 
-        holder.tempText.text = getItem(position)
+        holder.bind(getItem(position))
+        holder.apply {
+            btnEdit.setOnClickListener {
+                // TODO : todo 제목 수정 구현
+            }
+
+            btnDelete.setOnClickListener {
+                items.removeAt(position)
+                notifyDataSetChanged()
+
+                Toast.makeText(context, "${textTitle.text} 삭제되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         return view
     }
 
-    override fun getItem(position: Int): String = items[position]
+    override fun getItem(position: Int): Todo = items[position]
 
     override fun getItemId(position: Int): Long = 0L
 
