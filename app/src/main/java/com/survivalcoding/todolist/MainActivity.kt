1e5e8fcc59f7e8acd5b077f5b4f0696115e859e3
dale.band <dale.band@kakaocorp.com>
2021-01-05 22:28:21 +0900

    ToDo ListView생성

--- app/src/main/java/com/survivalcoding/todolist/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/todolist/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.survivalcoding.todolist
 
 import android.os.Bundle
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +9,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val todoList = findViewById<ListView>(R.id.todoList)
+        val editTitle = findViewById<ImageView>(R.id.editTitle)
+        val isDone = findViewById<RadioButton>(R.id.isDoneButton)
+
+        var data = listOf(
+                Todos(false, "Todo1"),
+                Todos(false, "Todo1"),
+                Todos(false, "Todo1"),
+                Todos(false, "Todo1"),
+                Todos(false, "Todo1"),
+        )
+
+        val todoAdapter = TodosAdapter(data)
+        todoList.adapter = todoAdapter
+
+        todoList.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, "$position 선택", Toast.LENGTH_SHORT).show()
+        }
+        
+        val addTodo = findViewById<Button>(R.id.addTodo)
+        addTodo.setOnClickListener { view ->
+            Toast.makeText(this, "할일 추가", Toast.LENGTH_SHORT).show()
+        }
     }
 }
