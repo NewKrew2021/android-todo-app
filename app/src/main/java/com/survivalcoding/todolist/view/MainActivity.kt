e98f2dc2a335a722decc6d321da4f173918d9de5
tony.mb <tony.mb@kakaocorp.com>
2021-01-06 19:04:45 +0900

    불필요한 코드 정리

--- app/src/main/java/com/survivalcoding/todolist/view/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/todolist/view/MainActivity.kt
@@ -2,7 +2,6 @@ package com.survivalcoding.todolist.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.survivalcoding.todolist.adapter.TodoListAdapter
 import com.survivalcoding.todolist.databinding.ActivityMainBinding
 import com.survivalcoding.todolist.model.TodoItem
@@ -10,7 +9,6 @@ import com.survivalcoding.todolist.model.TodoItem
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: TodoListAdapter
-    //    private lateinit var baseAdapter: TodoListBaseAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +31,5 @@ class MainActivity : AppCompatActivity() {
     private fun initializeView() {
         adapter = TodoListAdapter(mutableListOf())
         binding.toDoList.adapter = adapter
-        binding.toDoList.layoutManager = LinearLayoutManager(this)
     }
 }
