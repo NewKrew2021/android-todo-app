cfcf6468540a139fb75e99692d21ad17c8afc05f
bsgreentea1 <bsgreentea1@gmail.com>
2021-01-06 15:32:29 +0900

    리사이클러뷰 적용 - 리스트뷰에서 리사이클러뷰로 전환 - 뷰바인딩 유지

--- app/src/main/java/com/survivalcoding/todolist/adapter/TodoListAdapter.kt
+++ app/src/main/java/com/survivalcoding/todolist/adapter/TodoListAdapter.kt
@@ -2,45 +2,25 @@ package com.survivalcoding.todolist.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.survivalcoding.todolist.databinding.ItemTodoBinding
 
 class TodoListAdapter(
     private val context: Context,
     private val list: MutableList<TodoItem>
-) : BaseAdapter() {
+) : RecyclerView.Adapter<TodoViewHolder>() {
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-
-        val view: View
-        val holder: TodoViewHolder
-
-        if (convertView == null) {
-            val binding = ItemTodoBinding.inflate(LayoutInflater.from(context), parent, false)
-            view = binding.root
-
-            holder = TodoViewHolder(binding)
-
-            view.tag = holder
-        } else {
-            view = convertView
-            holder = view.tag as TodoViewHolder
-        }
-
-        holder.bind(getItem(position))
-
-        return view
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val binding = ItemTodoBinding.inflate(LayoutInflater.from(context), parent, false)
+        return TodoViewHolder(binding)
     }
 
-    override fun getItem(position: Int): TodoItem {
-        return list[position]
+    override fun getItemCount(): Int = list.size
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder.bind(list[position])
     }
-
-    override fun getItemId(position: Int): Long = position.toLong()
-
-    override fun getCount() = list.size
 
     fun addItem(item: TodoItem) {
         list.add(item)
--- app/src/main/java/com/survivalcoding/todolist/adapter/TodoViewHolder.kt
+++ app/src/main/java/com/survivalcoding/todolist/adapter/TodoViewHolder.kt
@@ -1,8 +1,11 @@
 package com.survivalcoding.todolist.adapter
 
+import androidx.recyclerview.widget.RecyclerView
 import com.survivalcoding.todolist.databinding.ItemTodoBinding
 
-class TodoViewHolder(val binding: ItemTodoBinding) {
+class TodoViewHolder(
+    private val binding: ItemTodoBinding
+) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: TodoItem) {
         binding.title.text = item.title
