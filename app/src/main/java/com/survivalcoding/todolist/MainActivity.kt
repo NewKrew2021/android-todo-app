32c837f9a2282c52f5d7257fedde8d7332d2087b
qufwnfahs <style_55@naver.com>
2021-01-06 15:32:36 +0900

    MainActivity ViewBinding 적용

--- app/src/main/java/com/survivalcoding/todolist/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/todolist/MainActivity.kt
@@ -1,39 +1,40 @@
 package com.survivalcoding.todolist
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.survivalcoding.todolist.adapter.Todo
 import com.survivalcoding.todolist.adapter.TodoAdapter
+import com.survivalcoding.todolist.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     private val items = mutableListOf<Todo>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val adapter = TodoAdapter(this, items)
 
-        val editTitle = findViewById<EditText>(R.id.editText_title)
+        binding.apply {
+            listView.adapter = adapter
 
-        findViewById<ListView>(R.id.list_view).adapter = adapter
-
-        findViewById<Button>(R.id.button_add).setOnClickListener {
-            if (editTitle.text.trim().isNotEmpty()) {
-                items.add(
-                    Todo(
-                        editTitle.text.toString(),
-                        Calendar.getInstance().time.toString()
+            buttonAdd.setOnClickListener {
+                if (editTextTitle.text.trim().isNotEmpty()) {
+                    items.add(
+                        Todo(
+                            editTextTitle.text.toString(),
+                            Calendar.getInstance().time.toString()
+                        )
                     )
-                )
-                adapter.notifyDataSetChanged()
+                    adapter.notifyDataSetChanged()
 
-                editTitle.text.clear()
+                    editTextTitle.text.clear()
+                }
             }
         }
     }
