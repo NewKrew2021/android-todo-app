cacdcb00b5c35d12334e72e4510e0fd7a4af2e08
tony.mb <tony.mb@kakaocorp.com>
2021-01-07 22:52:39 +0900

    시간값 초단위까지 저장하도록 변경

--- app/src/main/java/com/survivalcoding/todolist/util/DateUtil.kt
+++ app/src/main/java/com/survivalcoding/todolist/util/DateUtil.kt
@@ -3,9 +3,9 @@ package com.survivalcoding.todolist.util
 import java.text.SimpleDateFormat
 import java.util.*
 
-const val DATE_FORMAT_YYYY_MM_DD_HH_MM = "yyyy/MM/dd HH:mm"
+const val DATE_FORMAT_YYYY_MM_DD_HH_MM_SS = "yyyy/MM/dd HH:mm:ss"
 
 fun getCurrentTime(): String = SimpleDateFormat(
-    DATE_FORMAT_YYYY_MM_DD_HH_MM,
+    DATE_FORMAT_YYYY_MM_DD_HH_MM_SS,
     Locale.KOREA
 ).format(Date(System.currentTimeMillis()))
