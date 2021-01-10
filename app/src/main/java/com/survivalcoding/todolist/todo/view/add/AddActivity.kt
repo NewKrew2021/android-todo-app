49a1b237f99f3c81d94e826aa8d1e93b2a34e086
dale.band <dale.band@kakaocorp.com>
2021-01-10 18:40:27 +0900

    todo 추가를 별도 activity에서 처리 (intent수업 후 적용 예정)

---
+++ app/src/main/java/com/survivalcoding/todolist/todo/view/add/AddActivity.kt
@@ -0,0 +1,40 @@
+package com.survivalcoding.todolist.todo.view.add
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.survivalcoding.todolist.databinding.ActivityAddBinding
+import com.survivalcoding.todolist.todo.view.main.MainActivity
+import com.survivalcoding.todolist.todo.view.model.Todo
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AddActivity : AppCompatActivity() {
+    private lateinit var addBinding: ActivityAddBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        addBinding = ActivityAddBinding.inflate(layoutInflater)
+        setContentView(addBinding.root)
+
+        var dueDate = Date().time
+        val format = SimpleDateFormat(MainActivity.DATE_FORMAT)
+
+        addBinding.cancelButton.setOnClickListener { finish() }
+        addBinding.dueDate.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            val dateString = "$year-${month + 1}-$dayOfMonth"
+            dueDate = format.parse(dateString).time
+        }
+
+        addBinding.addTodo.setOnClickListener {
+            val todoString = addBinding.todoEditText.text.toString()
+            if (todoString.trim().isEmpty()) {
+                Toast.makeText(this, "할 일을 입력해 주세요", Toast.LENGTH_SHORT).show()
+            } else {
+                val item = Todo(false, todoString, dueDate)
+                val intent = Intent().putExtra(MainActivity.INTENT_KEY, item)
+            }
+        }
+    }
+}
