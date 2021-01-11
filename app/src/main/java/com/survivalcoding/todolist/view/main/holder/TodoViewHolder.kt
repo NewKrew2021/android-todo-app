7212fc05d73a8680a62217ecb9dcd6fdf10c1f17
qufwnfahs <style_55@naver.com>
2021-01-11 14:38:20 +0900

    완료(isDone) 시 수정 버튼 Invisible 및 title 취소선 적용 - updateTextPaintFlags() 생성

--- app/src/main/java/com/survivalcoding/todolist/view/main/holder/TodoViewHolder.kt
+++ app/src/main/java/com/survivalcoding/todolist/view/main/holder/TodoViewHolder.kt
@@ -1,5 +1,6 @@
 package com.survivalcoding.todolist.view.main.holder
 
+import android.graphics.Paint
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.survivalcoding.todolist.databinding.ItemTodoListBinding
@@ -20,23 +21,26 @@ class TodoViewHolder(private val binding: ItemTodoListBinding) :
             textViewTimes.text = todo.times
             checkBox.isChecked = todo.isDone
 
-            updateButtonsVisibility(todo.isOption)
+            updateButtonsVisibility(todo.isOption, todo.isDone)
+            updateTextPaintFlags(todo.isDone)
 
             checkBox.setOnClickListener {
                 todo.isDone = checkBox.isChecked
+                updateButtonsVisibility(todo.isOption, todo.isDone)
+                updateTextPaintFlags(todo.isDone)
                 updateListener.invoke()
             }
 
             layoutItem.setOnClickListener {
                 if (todo.isOption) {
                     todo.isOption = false
-                    updateButtonsVisibility(todo.isOption)
+                    updateButtonsVisibility(todo.isOption, todo.isDone)
                 }
             }
 
             buttonMenus.setOnClickListener {
                 todo.isOption = true
-                updateButtonsVisibility(todo.isOption)
+                updateButtonsVisibility(todo.isOption, todo.isDone)
             }
 
             buttonEdit.setOnClickListener {
@@ -53,11 +57,19 @@ class TodoViewHolder(private val binding: ItemTodoListBinding) :
         }
     }
 
-    private fun updateButtonsVisibility(isOption: Boolean) {
+    private fun updateButtonsVisibility(isOption: Boolean, isDone: Boolean) {
         binding.apply {
             buttonMenus.visibility = if (isOption) View.INVISIBLE else View.VISIBLE
-            buttonEdit.visibility = if (isOption) View.VISIBLE else View.INVISIBLE
+            buttonEdit.visibility = if (isOption && !isDone) View.VISIBLE else View.INVISIBLE
             buttonDelete.visibility = if (isOption) View.VISIBLE else View.INVISIBLE
+        }
+    }
+
+    private fun updateTextPaintFlags(isDone: Boolean) {
+        binding.apply {
+            textViewTitle.paintFlags =
+                if (isDone) (textViewTitle.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG)
+                else (textViewTitle.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()) // Paint.STRIKE_THRU_TEXT_FLAG(0x10) 만 제거하기 위한 코드
         }
     }
 }
