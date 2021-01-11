fdba08927bbda05e402f6c744ced12257c61f995
finn k <finn.k@kakaocorp.com>
2021-01-11 17:46:53 +0900

    - koin 추가

---
+++ app/src/main/java/com/survivalcoding/todolist/TodoApplication.kt
@@ -0,0 +1,16 @@
+package com.survivalcoding.todolist
+
+import android.app.Application
+import com.survivalcoding.todolist.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class TodoApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@TodoApplication)
+            modules(viewModelModule)
+        }
+    }
+}
---
+++ app/src/main/java/com/survivalcoding/todolist/di/ViewModelModule.kt
@@ -0,0 +1,8 @@
+package com.survivalcoding.todolist.di
+
+import org.koin.dsl.module
+
+
+val viewModelModule = module {
+
+}
