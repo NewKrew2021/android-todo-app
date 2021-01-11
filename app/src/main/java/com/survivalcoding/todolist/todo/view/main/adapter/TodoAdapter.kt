2a873f6ec915b74ce1083215452e9d391586aad5
dale.band <dale.band@kakaocorp.com>
2021-01-11 15:12:50 +0900

    Todo추가 삭제에 애니메이션 적용

--- app/src/main/java/com/survivalcoding/todolist/todo/view/main/adapter/TodoAdapter.kt
+++ app/src/main/java/com/survivalcoding/todolist/todo/view/main/adapter/TodoAdapter.kt
@@ -2,13 +2,13 @@ package com.survivalcoding.todolist.todo.view.main.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.ListAdapter
 import com.survivalcoding.todolist.databinding.ItemTodoBinding
 import com.survivalcoding.todolist.todo.data.TodoData
 import com.survivalcoding.todolist.todo.view.model.Todo
 import java.util.*
 
-class TodoAdapter() : RecyclerView.Adapter<TodoAdapterViewHolder>() {
+class TodoAdapter() : ListAdapter<Todo, TodoAdapterViewHolder>(TodoDiffCallback) {
     lateinit var itemTodoBinding: ItemTodoBinding
     private val model = TodoData
     private val currentTime = Date().time
@@ -19,39 +19,44 @@ class TodoAdapter() : RecyclerView.Adapter<TodoAdapterViewHolder>() {
         return TodoAdapterViewHolder(itemTodoBinding)
     }
 
-    override fun getItemCount() = model.getDataSize()
-
     override fun onBindViewHolder(holder: TodoAdapterViewHolder, position: Int) {
         val todoList = model.todoList
+        val holderPos = holder.adapterPosition
+
         itemTodoBinding.apply {
-            todoText.text = todoList[position].text
-            isDoneButton.isChecked = todoList[position].isDone
-            val dueDate = (todoList[position].dueDate - currentTime) / (24 * 60 * 60 * 1000)
+            todoText.text = todoList[holderPos].text
+            isDoneButton.isChecked = todoList[holderPos].isDone
+            val dueDate =
+                (todoList[holderPos].dueDate - currentTime) / (ONE_DAY_MILLISECONDS)
             dueDateText.text = "D - $dueDate"
 
             deleteTodoButton.setOnClickListener {
-                model.deleteTodo(todoList[position])
-                notifyDataSetChanged()
+                model.deleteTodo(todoList[holderPos])
+                submitList(model.todoList.toList())
             }
             isDoneButton.setOnClickListener {
-                model.doneTodo(holder.adapterPosition)
-                notifyDataSetChanged()
+                model.doneTodo(holderPos)
+                submitList(model.todoList.toList())
             }
         }
     }
 
     fun addTodo(todo: Todo) {
         model.addTodo(todo)
-        notifyDataSetChanged()
+        submitList(model.todoList.toList())
     }
 
     fun sortByTitle(order: Int) {
         model.sortByTitle(order)
-        notifyDataSetChanged()
+        submitList(model.todoList.toList())
     }
 
     fun sortByDueDate(order: Int) {
         model.sortByDate(order)
-        notifyDataSetChanged()
+        submitList(model.todoList.toList())
+    }
+
+    companion object {
+        const val ONE_DAY_MILLISECONDS = 24 * 60 * 60 * 1000 // 86_400_000
     }
 }
---
+++ app/src/main/java/com/survivalcoding/todolist/todo/view/main/adapter/TodoDiffCallback.kt
@@ -0,0 +1,15 @@
+package com.survivalcoding.todolist.todo.view.main.adapter
+
+import androidx.recyclerview.widget.DiffUtil
+import com.survivalcoding.todolist.todo.view.model.Todo
+
+object TodoDiffCallback : DiffUtil.ItemCallback<Todo>() {
+    override fun areItemsTheSame(oldItem: Todo, newItem: Todo): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Todo, newItem: Todo): Boolean {
+        return oldItem == newItem
+    }
+
+}
