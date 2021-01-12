48755f4a993ddea27d924e24fec8268690a0134d
bsgreentea1 <bsgreentea1@gmail.com>
2021-01-12 18:27:17 +0900

    프래그먼트 적용 및 전환 - FragmentFactory 추가 - 메인 액티비티에 메일 프래그먼트 추가 - AddActivity -> AddFragment로 전환 - Main, Add 페이지 각각 레이아웃 변경

--- app/src/main/java/com/survivalcoding/todolist/ui/main/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/todolist/ui/main/MainActivity.kt
@@ -1,107 +1,26 @@
 package com.survivalcoding.todolist.ui.main
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DefaultItemAnimator
-import com.survivalcoding.todolist.R
-import com.survivalcoding.todolist.adapter.TodoListAdapter
+import com.survivalcoding.todolist.TodoApp
 import com.survivalcoding.todolist.databinding.ActivityMainBinding
-import com.survivalcoding.todolist.model.TodoItem
-import com.survivalcoding.todolist.repository.TodoRepository
-import com.survivalcoding.todolist.ui.add.AddActivity
-import java.util.*
+import com.survivalcoding.todolist.factory.MainFragmentFactory
+import com.survivalcoding.todolist.utils.NavigationUtil
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var adapter: TodoListAdapter
     private lateinit var binding: ActivityMainBinding
-    private val todoRepository =
-        TodoRepository()
+    private val todoRepository by lazy { (application as TodoApp).todoRepository }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        supportFragmentManager.fragmentFactory = MainFragmentFactory(todoRepository)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.search.setOnClickListener {
-            updateUi(todoRepository.getFilteredItems(binding.input.text.toString()))
-        }
-
-        adapter = TodoListAdapter(
-            sort = { todoRepository.sortItems() },
-            remove = { targetItem -> todoRepository.removeItem(targetItem) },
-            update = { updateUi() }
-        )
-
-        binding.list.apply {
-            adapter = this@MainActivity.adapter
-            addItemDecoration(VerticalSpaceItemDecoration(ITEM_VERTICAL_INTERVAL))
-            itemAnimator = DefaultItemAnimator().apply {
-                changeDuration = 100
-                moveDuration = 100
-            }
+        if (savedInstanceState == null) {
+            NavigationUtil.openMainFragment(supportFragmentManager)
         }
     }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == ADD_REQUEST_CODE) {
-            data?.let { resultIntent ->
-                resultIntent.extras?.getParcelable<TodoItem>(AddActivity.NEW_TODO)?.let {
-                    todoRepository.addItem(it)
-                    updateUi()
-                }
-            }
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_actionbar, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.search -> {
-                startActivityForResult(Intent(this, AddActivity::class.java), ADD_REQUEST_CODE)
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-
-        outState.putParcelableArrayList(SAVED_LIST_KEY, todoRepository.list as ArrayList<TodoItem>)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-
-        savedInstanceState.getParcelableArrayList<TodoItem>(SAVED_LIST_KEY)?.let {
-            todoRepository.resetItems(it)
-            updateUi()
-        }
-    }
-
-    private fun updateUi() {
-        adapter.submitList(todoRepository.list.toList())
-    }
-
-    private fun updateUi(list: List<TodoItem>) {
-        adapter.submitList(list)
-    }
-
-    companion object {
-        private const val ITEM_VERTICAL_INTERVAL = 12
-        private const val SAVED_LIST_KEY = "SAVED_LIST_KEY"
-        private const val ADD_REQUEST_CODE = 100
-    }
-
 }
---
+++ app/src/main/java/com/survivalcoding/todolist/ui/main/MainFragment.kt
@@ -0,0 +1,100 @@
+package com.survivalcoding.todolist.ui.main
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DefaultItemAnimator
+import com.survivalcoding.todolist.R
+import com.survivalcoding.todolist.adapter.TodoListAdapter
+import com.survivalcoding.todolist.databinding.FragmentMainBinding
+import com.survivalcoding.todolist.model.TodoItem
+import com.survivalcoding.todolist.repository.TodoRepository
+import com.survivalcoding.todolist.utils.NavigationUtil
+
+class MainFragment(private val todoRepository: TodoRepository) : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var adapter: TodoListAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.search.setOnClickListener {
+            updateUi(todoRepository.getFilteredItems(binding.input.text.toString()))
+        }
+
+        adapter = TodoListAdapter(
+            sort = { todoRepository.sortItems() },
+            remove = { targetItem -> todoRepository.removeItem(targetItem) },
+            update = { updateUi() }
+        )
+
+        binding.list.apply {
+            adapter = this@MainFragment.adapter
+            addItemDecoration(VerticalSpaceItemDecoration(ITEM_VERTICAL_INTERVAL))
+            itemAnimator = DefaultItemAnimator().apply {
+                changeDuration = 100
+                moveDuration = 100
+            }
+        }
+
+        setHasOptionsMenu(true)
+        updateUi()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, menuInflater)
+        menuInflater.inflate(R.menu.menu_actionbar, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.add_button -> {
+                NavigationUtil.openAddFragment(parentFragmentManager)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun updateUi() {
+
+        if (binding.input.text.toString().isNotBlank()) {
+            updateUi(todoRepository.getFilteredItems(binding.input.text.toString()))
+        } else {
+            adapter.submitList(todoRepository.list.toList())
+        }
+    }
+
+    private fun updateUi(list: List<TodoItem>) {
+        adapter.submitList(list)
+    }
+
+    companion object {
+        private const val ITEM_VERTICAL_INTERVAL = 12
+    }
+}
