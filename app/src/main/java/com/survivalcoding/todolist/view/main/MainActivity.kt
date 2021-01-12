89a3d2dea81f03cc03016465523fa51cc921c480
qufwnfahs <style_55@naver.com>
2021-01-12 16:41:39 +0900

    Add FragmentFactory - FragmentFactory를 이용해서 생성자로 viewModel 인스턴스 전달

--- app/src/main/java/com/survivalcoding/todolist/view/main/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/todolist/view/main/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.commit
 import com.survivalcoding.todolist.R
 import com.survivalcoding.todolist.data.TodoViewModel
 import com.survivalcoding.todolist.databinding.ActivityMainBinding
+import com.survivalcoding.todolist.factory.TodoFragmentFactory
 import com.survivalcoding.todolist.view.main.fragment.MainFragment
 import com.survivalcoding.todolist.view.main.model.Todo
 
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        supportFragmentManager.fragmentFactory = TodoFragmentFactory(viewModel)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -23,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
-                add(R.id.fragment_container_view, MainFragment(viewModel))
+                add(R.id.fragment_container_view, MainFragment::class.java, null)
             }
         }
     }
