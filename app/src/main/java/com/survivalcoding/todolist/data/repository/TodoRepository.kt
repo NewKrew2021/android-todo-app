87036d7f1f58a011820302c06c762c8ff1a78fa9
bsgreentea1 <bsgreentea1@gmail.com>
2021-01-15 17:43:10 +0900

    메모리 레포지토리 수정

--- app/src/main/java/com/survivalcoding/todolist/data/repository/TodoRepository.kt
+++ app/src/main/java/com/survivalcoding/todolist/data/repository/TodoRepository.kt
@@ -1,28 +1,37 @@
 package com.survivalcoding.todolist.data.repository
 
+import com.survivalcoding.todolist.data.DefaultTodoRepository
 import com.survivalcoding.todolist.model.TodoItem
+import java.util.concurrent.atomic.AtomicInteger
 
-class TodoRepository {
+class TodoRepository : DefaultTodoRepository {
+
+    var id = AtomicInteger(0)
 
     private val _list = mutableListOf<TodoItem>()
     val list: List<TodoItem> = _list
-    fun addItem(item: TodoItem) {
-        _list.add(0, item)
+
+    override fun addItem(item: TodoItem) {
+        _list.add(0, item.apply { id = this@TodoRepository.id.getAndIncrement() })
     }
 
-    fun resetItems(items: MutableList<TodoItem>) {
-        _list.clear()
-        _list.addAll(items)
-    }
-
-    fun removeItem(targetItem: TodoItem) {
+    override fun removeItem(targetItem: TodoItem) {
         _list.takeIf { it.contains(targetItem) }?.remove(targetItem)
     }
 
-    fun sortItems() {
-        _list.sortWith(compareBy({ it.isChecked }, { -it.timeStamp }))
+    override fun updateItem(item: TodoItem) {
+        _list.forEach {
+            if (it.id == item.id) _list.remove(it)
+        }
+        _list.add(item)
     }
 
-    fun getFilteredItems(keyword: String): List<TodoItem> =
-        if (keyword.isNotBlank()) _list.filter { it.title.contains(keyword) }.map { it } else list
+    override fun getOrderedItems(): List<TodoItem> {
+        return list.sortedWith(compareBy({ it.isChecked }, { -it.timeStamp }))
+    }
+
+    override fun getFilteredItemsBy(keyword: String): List<TodoItem> {
+        return if (keyword.isNotBlank()) _list.filter { it.title.contains(keyword) }
+            .map { it } else list
+    }
 }
